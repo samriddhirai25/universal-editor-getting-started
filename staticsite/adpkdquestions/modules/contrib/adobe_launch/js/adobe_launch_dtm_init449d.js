cb0202d8ac4fb4eef17aa2fcf72013d4ff372964
Samriddhi2503 <Samriddhi.Rai@gds.ey.com>
2025-12-09 19:26:37 +0530

    deleted a folder

--- staticsite/adpkdquestions/modules/contrib/adobe_launch/js/adobe_launch_dtm_init449d.js
+++
@@ -1,7 +0,0 @@
-/**
- * @file: adobe_launch_dtm_init.js
- * @description: initialization script for Adobe DTM event tracking objects
- */
-
-window.digitalData = { events: [] };
-window.DTM_DATA = window.DTM_DATA || [];
