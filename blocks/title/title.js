d41c0f557a9328e20d4174361e8120158a069a55
Samriddhi2503 <Samriddhi.Rai@gds.ey.com>
2025-12-17 20:12:37 +0530

    updated title comp

--- blocks/title/title.js
+++
@@ -1,5 +0,0 @@
-export default async function decorate(block) {
-  const colorStyle = block.children[1].textContent.trim();
-  block.children[0].classList.add(colorStyle);
-  block.children[1].remove();
-}
