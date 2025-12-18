5c8e97fcd7b716b3928b71e5f8125f0ca89347ef
Samriddhi2503 <Samriddhi.Rai@gds.ey.com>
2025-12-18 17:38:22 +0530

    fixed aem js

--- scripts/aem.js
+++ scripts/aem.js
@@ -688,35 +688,6 @@ async function loadSections(element) {
 }
 
 init();
-export default function decorate(block) {
-  const rows = [...block.children];
-
-  const iconWrapper = rows[0];
-  const contentWrapper = rows[1];
-
-  const icon = iconWrapper.querySelector('img');
-  const text = contentWrapper.querySelector('p');
-  const link = contentWrapper.querySelector('a');
-
-  if (!link || !text) return;
-
-  const button = document.createElement('a');
-  button.href = link.href;
-  button.className = 'btn';
-
-  if (icon) {
-    icon.classList.add('btn-icon');
-    button.append(icon);
-  }
-
-  const span = document.createElement('span');
-  span.textContent = text.textContent;
-  button.append(span);
-
-  block.textContent = '';
-  block.append(button);
-}
-
  
 
 export {
